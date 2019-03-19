97caa13cd26df734834d19803051806a798f92e2
Carlos Ramos <carlos.ramos@decoracontent.com>
2019-03-19 15:38:03 -0300

    Refactoring, added comment

--- task.py
+++ task.py
@@ -2,6 +2,9 @@ from celery import Celery
 
 app = Celery('tasks', backend='redis://localhost', broker='redis://localhost:6379/0')
 
-@app.task
-def add(x, y):
-    return x + y
+@app.task(bind=True)
+def add(self,x, y):
+    val = x + y
+    # self.update_state(state='STARTED', meta={'value':'Loaded'})
+    val = val + val
+    return val
